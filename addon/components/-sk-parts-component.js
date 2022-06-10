07d593a5599b60a41b7a873115bd268cc5fac07a
James Hill <james@onehilltech.com>
2022-06-10 00:44:24 -0400

    feat: remove dependency on lodash

--- addon/components/-sk-parts-component.js
+++ addon/components/-sk-parts-component.js
@@ -1,11 +1,10 @@
 import SkComponent from "./-sk-component";
-import { isArray } from 'lodash-es';
 
 export default class SkPartsComponent extends SkComponent {
   defaultPartsCount = 5;
 
   get parts () {
     let parts = this.args.parts || this.defaultPartsCount;
-    return isArray (parts) ? parts : new Array (parts);
+    return Array.isArray (parts) ? parts : new Array (parts);
   }
 }
